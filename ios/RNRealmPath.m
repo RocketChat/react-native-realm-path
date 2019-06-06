eb8c2531af7b542ff723d19fd606aecadb416930
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-06-06 18:58:37 -0300

    :art: Add / to path iOS

--- ios/RNRealmPath.m
+++ ios/RNRealmPath.m
@@ -6,7 +6,7 @@
 - (NSDictionary *)constantsToExport
 {
   NSURL *realmPathUrl = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.chat.rocket.reactnative"];
-  NSString *realmPath = [[realmPathUrl path] stringByAppendingPathComponent:@""];
+  NSString *realmPath = [[realmPathUrl path] stringByAppendingPathComponent:@"/"];
   return @{@"realmPath" : realmPath};
 }
 RCT_EXPORT_MODULE()
