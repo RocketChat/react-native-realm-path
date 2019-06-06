9f49f0a14ed77922ddc152ece524db304da7c360
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-06-06 18:54:13 -0300

    :bug: Fix imports on Android Path

--- android/src/main/java/com/reactlibrary/RNRealmPathModule.java
+++ android/src/main/java/com/reactlibrary/RNRealmPathModule.java
@@ -6,6 +6,9 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class RNRealmPathModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
@@ -23,7 +26,7 @@ public class RNRealmPathModule extends ReactContextBaseJavaModule {
   @Override
   public Map<String, Object> getConstants() {
     final Map<String, Object> constants = new HashMap<>();
-    constants.put('realmPath', '');
+    constants.put("realmPath", "");
     return constants;
   }
 }
