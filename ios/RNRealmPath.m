7ea9b88a19dcc05f2df40d720263d0af1231072d
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-06-06 19:10:52 -0300

    :bug: Fix / on iOS Path

--- ios/RNRealmPath.m
+++ ios/RNRealmPath.m
@@ -6,10 +6,9 @@
 - (NSDictionary *)constantsToExport
 {
   NSURL *realmPathUrl = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.chat.rocket.reactnative"];
-  NSString *realmPath = [[realmPathUrl path] stringByAppendingPathComponent:@"/"];
+  NSString *realmPath = [[realmPathUrl path] stringByAppendingString:@"/"];
   return @{@"realmPath" : realmPath};
 }
 RCT_EXPORT_MODULE()
 
 @end
-  
